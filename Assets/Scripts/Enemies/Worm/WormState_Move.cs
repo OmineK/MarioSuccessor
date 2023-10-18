e34f92bbb631e21be73c42719473f66dd0813c85
Oskar Szarmach <szarmach.o@gmail.com>
2023-10-18 14:43:38 +0200

    (Added) Worm enemy // (Added) Worm enemy move state // (Added) Grounded enemy layer

---
+++ Assets/Scripts/Enemies/Worm/WormState_Move.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WormState_Move : EnemyState
+{
+    Worm wormEnemy;
+
+    public WormState_Move(Enemy _enemy, EnemyStateMachine _stateMachine, string _animBoolName, Worm _wormEnemy) : base(_enemy, _stateMachine, _animBoolName)
+    {
+        this.wormEnemy = _wormEnemy;
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+    }
+
+    public override void FixedUpdate()
+    {
+        base.FixedUpdate();
+
+        rb.velocity = new Vector3(wormEnemy.moveSpeed * wormEnemy.facingDir, rb.velocity.y);
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        if (enemy.isWallDetected() || !enemy.isGroundDetected())
+            enemy.Flip();
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+}
