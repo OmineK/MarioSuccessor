90e2393735420550b4b425fb6e9b5e0935c80f92
Oskar Szarmach <szarmach.o@gmail.com>
2023-11-11 15:36:34 +0100

    (Added) Best game score to main menu

--- Assets/Scripts/MenuUI.cs
+++ Assets/Scripts/MenuUI.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuUI : MonoBehaviour
 {
+    [SerializeField] TextMeshProUGUI bestScore;
+
     void Start()
     {
         Time.timeScale = 1;
+
+        bestScore.text = "Best game score: " + PlayerPrefs.GetInt("bestScore");
     }
 
     public void StartGameButton()
